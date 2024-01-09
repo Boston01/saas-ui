6316b01bd564b29f6d5af5cf43bc47cd2cf28647
ZhengjunHUO <firelouiszj@hotmail.com>
2024-01-09 15:55:31 +0100

    Use built-in crypto package
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,17 +1,31 @@
-import Cryptr from "cryptr";
+const crypto = require("crypto");
 
 export function encrypt(text) {
-    const secretKey = process.env.NEXTAUTH_SECRET;
-    const cryptr = new Cryptr(secretKey);
+  // The string length of NEXTAUTH_SECRET should be 32 to be qualified for aes-256-gcm
+  const secretKey = process.env.NEXTAUTH_SECRET;
 
-    const encryptedString = cryptr.encrypt(text);
-    return encryptedString;
+  const iv = new crypto.randomBytes(12);
+  const cipher = crypto.createCipheriv("aes-256-gcm", secretKey, iv);
+
+  let enc_utf = cipher.update(text, "utf8");
+  let enc_buf = cipher.final();
+  let auth_tag = cipher.getAuthTag();
+
+  return Buffer.concat([enc_utf, enc_buf, iv, auth_tag]).toString("hex");
 }
 
 export function decrypt(encryptedString) {
-    const secretKey = process.env.NEXTAUTH_SECRET;
-    const cryptr = new Cryptr(secretKey);
+  const secretKey = process.env.NEXTAUTH_SECRET;
 
-    const text = cryptr.decrypt(encryptedString);
-    return text;
+  let enc = Buffer.from(encryptedString, "hex");
+  const iv = enc.subarray(enc.length - 28, enc.length - 16);
+  const auth_tag = enc.subarray(enc.length - 16);
+  const enc_data = enc.subarray(0, enc.length - 28);
+
+  const decipher = crypto.createDecipheriv("aes-256-gcm", secretKey, iv);
+  decipher.setAuthTag(auth_tag);
+
+  let rslt = decipher.update(enc_data, null, "utf8");
+  rslt += decipher.final("utf8");
+  return rslt;
 }
